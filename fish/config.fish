10dcb9ba8f86617a87b5ccb91c84868e85d767e2
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-11-19 18:11:38 +1100

    Add Homebrew for linux to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,21 @@ else
     set -gx EDITOR vim
 end
 
+# Homebrew on Linux
+if test (uname) != 'Darwin'
+    set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
+    set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar"
+    set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew"
+    set -g fish_user_paths \
+        "/home/linuxbrew/.linuxbrew/bin" \
+        "/home/linuxbrew/.linuxbrew/sbin" \
+        $fish_user_paths
+    set -q MANPATH; or set MANPATH ''; set -gx MANPATH \
+        "/home/linuxbrew/.linuxbrew/share/man" $MANPATH
+    set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH \
+        "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH
+end
+
 # Abbreviations
 source ~/.config/fish/abbr.fish
 
