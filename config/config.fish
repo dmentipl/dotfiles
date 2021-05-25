e1ac920f2becc7bbed230b2c5521bd16f120ed96
Daniel Mentiplay <daniel.mentiplay@bom.gov.au>
2021-05-25 14:08:04 +1000

    Simplify shell configuration

--- config/config.fish
+++ config/config.fish
@@ -1,28 +1,20 @@
-# -- ENVIRONMENT --
+# ============================================================================ #
+# ==== CONFIGURATION ==== #
 
 set -gx PATH ~/.local/bin $PATH
 set -gx PATH ~/bin $PATH
 
 command -v nvim > /dev/null && set -gx EDITOR nvim || set -gx EDITOR vim
 
-# -- CONFIGURATION --
-
 test -e ~/conda/bin/conda && eval ~/conda/bin/conda "shell.fish" "hook" $argv | source
 function fish_right_prompt; end
 
 command -v starship > /dev/null && starship init fish | source
 
-bind \eg accept-autosuggestion
-
-test (uname) = 'Darwin' && \
-  test -e ~/.iterm2_shell_integration.fish && \
-  source ~/.iterm2_shell_integration.fish
-
-# -- LOCAL --
-
 test -e ~/.config/fish/local.fish && source ~/.config/fish/local.fish
 
-# -- ABBREVIATIONS --
+# ============================================================================ #
+# ==== ABBREVIATIONS ==== #
 
 abbr --add --global chown 'chown -R'
 abbr --add --global cp 'cp -i -r'
@@ -40,79 +32,79 @@ abbr --add --global ssh 'ssh -Y'
 abbr --add --global which 'which -a'
 
 if command -v trash > /dev/null
-    abbr --add --global bin trash
-    abbr --add --global rm 'printf "%s\n" "Use bin instead, or \\rm for real rm if required."'
+  abbr --add --global bin trash
+  abbr --add --global rm 'printf "%s\n" "Use bin instead, or \\rm for real rm if required."'
 else
-    abbr --add --global rm 'rm -R'
+  abbr --add --global rm 'rm -R'
 end
 
 if command -v exa > /dev/null
-    abbr --add --global l 'exa -l -h'
-    abbr --add --global ls 'exa -1'
-    abbr --add --global lg 'exa -l -h --git'
-    abbr --add --global ll 'exa -l -h -a'
-    abbr --add --global lm 'exa -l -h -s modified'
-    abbr --add --global lr 'exa -l -h -R'
-    abbr --add --global lt 'exa -l -h -T'
-    abbr --add --global lx 'exa -l -h -s extension'
-    abbr --add --global lz 'exa -l -h -s size'
-    abbr --add --global tree 'exa -T'
+  abbr --add --global l 'exa -l -h'
+  abbr --add --global ls 'exa -1'
+  abbr --add --global lg 'exa -l -h --git'
+  abbr --add --global ll 'exa -l -h -a'
+  abbr --add --global lm 'exa -l -h -s modified'
+  abbr --add --global lr 'exa -l -h -R'
+  abbr --add --global lt 'exa -l -h -T'
+  abbr --add --global lx 'exa -l -h -s extension'
+  abbr --add --global lz 'exa -l -h -s size'
+  abbr --add --global tree 'exa -T'
 else
-    abbr --add --global ls 'ls -1'
-    abbr --add --global l 'ls -lh'
-    abbr --add --global la 'ls -lah'
-    abbr --add --global ll 'ls -lAh'
-    abbr --add --global lx 'ls -lAh -XB'
-    abbr --add --global lk 'ls -lAh -Sr'
-    abbr --add --global lm 'ls -lAh -tr'
+  abbr --add --global ls 'ls -1'
+  abbr --add --global l 'ls -lh'
+  abbr --add --global la 'ls -lah'
+  abbr --add --global ll 'ls -lAh'
+  abbr --add --global lx 'ls -lAh -XB'
+  abbr --add --global lk 'ls -lAh -Sr'
+  abbr --add --global lm 'ls -lAh -tr'
 end
 
 if command -v git > /dev/null
-    abbr --add --global ga 'git add'
-    abbr --add --global gam 'git add --update'
-    abbr --add --global gap 'git add --patch'
-    abbr --add --global gb 'git branch'
-    abbr --add --global go 'git checkout'
-    abbr --add --global gc 'git commit --verbose'
-    abbr --add --global gd 'git diff'
-    abbr --add --global gds 'git diff --staged'
-    abbr --add --global gf 'git fetch --prune'
-    abbr --add --global gg 'git log --stat --abbrev-commit'
-    abbr --add --global ggo 'git log --oneline --graph --abbrev-commit'
-    abbr --add --global ggp 'git log --patch --abbrev-commit'
-    abbr --add --global gl 'git pull'
-    abbr --add --global gp 'git push'
-    abbr --add --global gr 'git rebase'
-    abbr --add --global gm 'git remote --verbose'
-    abbr --add --global gmu 'git remote add upstream'
-    abbr --add --global gx 'git reset --soft HEAD^'
-    abbr --add --global gt 'git stash --include-untracked'
-    abbr --add --global gs 'git status'
+  abbr --add --global ga 'git add'
+  abbr --add --global gam 'git add --update'
+  abbr --add --global gap 'git add --patch'
+  abbr --add --global gb 'git branch'
+  abbr --add --global go 'git checkout'
+  abbr --add --global gc 'git commit --verbose'
+  abbr --add --global gd 'git diff'
+  abbr --add --global gds 'git diff --staged'
+  abbr --add --global gf 'git fetch --prune'
+  abbr --add --global gg 'git log --stat --abbrev-commit'
+  abbr --add --global ggo 'git log --oneline --graph --abbrev-commit'
+  abbr --add --global ggp 'git log --patch --abbrev-commit'
+  abbr --add --global gl 'git pull'
+  abbr --add --global gp 'git push'
+  abbr --add --global gr 'git rebase'
+  abbr --add --global gm 'git remote --verbose'
+  abbr --add --global gmu 'git remote add upstream'
+  abbr --add --global gx 'git reset --soft HEAD^'
+  abbr --add --global gt 'git stash --include-untracked'
+  abbr --add --global gs 'git status'
 end
 
 if command -v tmux > /dev/null
-    abbr --add --global ta 'tmux attach'
-    abbr --add --global tc 'tmux choose-tree -s'
-    abbr --add --global td 'tmux detach'
-    abbr --add --global tl 'tmux list-sessions'
-    abbr --add --global tk 'tmux kill-session'
-    abbr --add --global tn 'tmux new-session'
-    abbr --add --global ts 'tmux switch-client'
+  abbr --add --global ta 'tmux attach'
+  abbr --add --global tc 'tmux choose-tree -s'
+  abbr --add --global td 'tmux detach'
+  abbr --add --global tl 'tmux list-sessions'
+  abbr --add --global tk 'tmux kill-session'
+  abbr --add --global tn 'tmux new-session'
+  abbr --add --global ts 'tmux switch-client'
 end
 
 if command -v nvim > /dev/null
-    abbr --add --global vim nvim
-    abbr --add --global ex 'nvim -e'
-    abbr --add --global view 'nvim -R'
-    abbr --add --global vimdiff 'nvim -d'
+  abbr --add --global vim nvim
+  abbr --add --global ex 'nvim -e'
+  abbr --add --global view 'nvim -R'
+  abbr --add --global vimdiff 'nvim -d'
 end
 
 if command -v pbcopy > /dev/null
-    abbr --add --global copy 'pbcopy'
-    abbr --add --global paste 'pbpaste'
+  abbr --add --global copy 'pbcopy'
+  abbr --add --global paste 'pbpaste'
 else if command -v xsel > /dev/null
-    abbr --add --global copy 'xsel --clipboard --input'
-    abbr --add --global paste 'xsel --clipboard --output'
+  abbr --add --global copy 'xsel --clipboard --input'
+  abbr --add --global paste 'xsel --clipboard --output'
 end
 
 command -v xdg-open > /dev/null && abbr --add --global open xdg-open
