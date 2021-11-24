1f9f0979c75f5dd74ab2b660f1fa4ba9cadc4dee
Daniel Mentiplay <daniel.mentiplay@protonmail.com>
2021-11-24 17:34:18 +1100

    Update setup.sh for Python

--- dev/python/setup.sh
+++ dev/python/setup.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 #
-# Install Mambaforge conda at ~/conda.
-# From https://github.com/conda-forge/miniforge.
+# Install Mambaforge conda at ~/conda from
+# https://github.com/conda-forge/miniforge.
 #
-# Conda needs to be initialized. See conda-init.fish.
+# After installation Conda needs to be initialized to be used.
+# See conda-init.fish.
 
+# Get OS
 if [[ $(uname) == Linux ]]; then
     OS=Linux
 elif [[ $(uname) == Darwin ]]; then
     OS=MacOSX
 fi
+
+# Download and run installer
 INSTALLER="Mambaforge-$OS-x86_64.sh"
 URL="https://github.com/conda-forge/miniforge/releases/latest/download/$INSTALLER"
 wget "$URL" && bash "$INSTALLER" -b -p "$HOME/conda" && rm "$INSTALLER"
+
+# Activate base and install packages
+conda-init
+mamba install --file environment.yml
